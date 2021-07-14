78052970f692876ebe979249d5418c6cf608744c
Darrin Cecil <darrinthomascecil@gmail.com>
2021-07-14 10:01:03 -0400

    playin around

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace new_web_api
 {
     public class Program
     {
-        //running a simple test
+        //This change came from Github
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
