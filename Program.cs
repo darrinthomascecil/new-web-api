3c9663fd079f91d774338ceede7fea9dae60449e
Darrin Cecil <darrinthomascecil@gmail.com>
2021-07-14 09:47:44 -0400

    running simple test

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace new_web_api
 {
     public class Program
     {
+        //running a simple test
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
